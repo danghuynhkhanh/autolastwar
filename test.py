0b6461fcae2cba8afe4e614b98b008c7263a9c1c
danghuynhkhanh <danghuynhkhanh@gmail.com>
2025-08-01 12:35:14 +0700

    up

---
+++ test.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+import hashlib
+
+def generate_key(machine_id, days_valid):
+    expiry_date = (datetime.now() + timedelta(days=days_valid)).strftime('%Y-%m-%d')
+    raw_data = f"{machine_id}|{expiry_date}"
+    hash_part = hashlib.sha256(raw_data.encode()).hexdigest()[:16].upper()
+    return f"{hash_part}-{expiry_date}"
+
+print(generate_key("12345678", 30))
